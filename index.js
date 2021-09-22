d24fd93458b97d888be1dbf34e9d2119c6d6f5ce
AustinRS016 <austinsantamaria@gmail.com>
2021-09-21 23:31:12 -0700

    change logo size

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var map = new mapboxgl.Map({
   zoom: 2, // starting zoom
 });
 
-
+//** team = "team_Name.png" | teamName = "team Name" **\\
 function addImages(team, teamName){
-  map.loadImage('https://raw.githubusercontent.com/AustinRS016/soccerMap/master/Logos/' + team, (error, image) => {
+  map.loadImage('https://raw.githubusercontent.com/AustinRS016/soccerMap/master/signs/' + team, (error, image) => {
     if (error) throw error;
     console.log(image)
     map.addImage(teamName, image);
@@ -28,70 +28,27 @@ function addImages(team, teamName){
             },
         "filter": ['==', 'Club', teamName]
         });
-});
-}
-
+    });
+  }
 
 map.on('load', function(){
-
-d3.json("https://raw.githubusercontent.com/AustinRS016/soccerMap/master/Clubs.json", function(json) {
-
-var l = json['Club'].length
-var i = 0;
-
-for (i = 0; i < l; i++ ){
-  var team = json['Club'][i]
-  // console.log(team)
-  var teamName = team.replace(/_/g, ' ')
-  teamName = teamName.substring(0, teamName.length -4);
-  var img = teamName + 'Img'
-  // console.log(teamName)
-  addImages(team,teamName)
-}
-})
-})
+    d3.json("https://raw.githubusercontent.com/AustinRS016/soccerMap/master/Clubs.json", function(json) {
+    var l = json['Club'].length
+    var i = 0;
+    for (i = 0; i < l; i++ ){
+        var team = json['Club'][i]
+        console.log(team)
+        var teamName = team.replace(/_/g, ' ')
+        teamName = teamName.substring(0, teamName.length -4);
+        var img = teamName + 'Img'
+        // console.log(teamName)
+        addImages(team,teamName)
+        }
+      })
+    })
 
 
 
-// map.on('load', function(){
-//
-//       d3.json("https://raw.githubusercontent.com/AustinRS016/soccerMap/master/Clubs.json", function(json) {
-//
-//       var l = json['Club'].length
-//       var i = 0;
-//
-//         for (i = 0; i < l; i++ ){
-//           var team = json['Club'][i]
-//           console.log(team)
-//           var teamName = team.replace(/_/g, ' ')
-//           teamName = teamName.substring(0, teamName.length -4);
-//           var img = teamName + 'Img'
-//           console.log(teamName)
-//
-//           map.loadImage('https://raw.githubusercontent.com/AustinRS016/soccerMap/master/Logos/' + team, (error, image) => {
-//               if (error) throw error;
-//               console.log(image)
-//               map.addImage(teamName, image);
-//               map.addSource(team,{
-//                        "type": "geojson",
-//                        "data": "jsons/bundesliga.geojson"
-//                    });
-//               map.addLayer({
-//                  "id":team,
-//                   "type":"symbol",
-//                   "source":teamName,
-//                   "layout": {
-//                     'icon-image': 'custom',
-//                     'icon-size': 0.01,
-//                       },
-//                   "filter": ['==', 'Club', teamName]
-//                   });
-//
-//           });
-//         }
-//       })
-//
-//
 // map.on('mousemove', 'bundesliga', (e) => {
 //   map.getCanvas().style.cursor = 'pointer';
 // });
@@ -101,5 +58,3 @@ for (i = 0; i < l; i++ ){
 // map.on('click', 'bundesliga', function(e){
 //   window.open('https://www.lemonparty.org', '_blank').focus();
 // });
-//
-//     });
